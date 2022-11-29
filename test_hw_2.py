336dc23043b77dce780fc5347342167e1b24f6ac
Ксения <118900137+Lisarogoza@users.noreply.github.com>
2022-11-29 12:34:13 +0300

    Fixed assertion in negative test of test_hw_2 file

--- test_hw_2.py
+++ test_hw_2.py
@@ -8,5 +8,5 @@ def test_google_search_positive(open_browser):
 
 
 def test_google_search_negative(open_browser):
-    browser.element('[name=q]').should(be.blank).type('selene').press_enter()
-    browser.element('[id=search]').should(have.text('yashaka/silen: User-oriented Web UI browser tests in Python'))
+    browser.element('[name=q]').should(be.blank).type('rtbhrjhvtr5645v56vyrtybtyu7').press_enter()
+    browser.element('#res').should(have.text('Your search - rtbhrjhvtr5645v56vyrtybtyu7 - did not match any documents.'))
